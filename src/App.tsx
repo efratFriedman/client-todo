7c1790098b85180fa3cf09678a27e13d1c65dca3
Efrat Friedman <efratfriedman18@gmail.com>
2026-03-05 12:04:32 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import Header from './components/Header/Header'
 import { useUiStore } from './stores/uiStore'
 import AddTask from './components/AddTask/AddTask'
 import TaskList from './components/TaskList/TaskList'
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   const { theme } = useUiStore()
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <div className={`app ${theme}`}>
+      <ToastContainer position='top-right' autoClose={3000}/>
       <Header />
       <div className="tasks-container">
         <TaskList />
